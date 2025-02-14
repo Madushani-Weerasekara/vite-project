080affae4a22720dce93158614560d7057f6e692
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-02-14 23:00:17 +0530

    Updated with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import ListGroup from "./components/ListGroup";
+import React from 'react'
 
-function App() {
-  const items = ["San francisco", "Tokyo", "Landon", "New York", "Texsas", ]
-return ( 
-  <>
-    <h1>List</h1>
-    <ul className ="list-group">
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-    </ul>
-  </> 
-); 
+export default function App() {
+  return (
+    <div id='wrapper'>
 
-} 
+      <div id='navcontent'>
+        <h1>Header Content</h1>
+      </div>
 
-export default App;
+      <div id='bodycontent'>
+        <p>Main Content</p>
+      </div>
+
+      <div id='footercontent'>
+        <p>Footer Content</p>
+      </div>
+
+    </div>
+  )
+}
+
+ 
---
+++ src/Components/BodyComponents/BodyComponents.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BodyComponents() {
+  return (
+    <div>BodyComponents</div>
+  )
+}
