5bc8898f2c20a8995ce543c7c182f5286f5a6ce5
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-02-18 16:30:14 +0530

    React Hooks: useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderContent from './Components/HeaderContent/HeaderContent';
 import BodyContent from './Components/BodyContent/BodyContent';
 import FooterContent from './Components/FooterContent/FooterContent';
@@ -11,7 +11,12 @@ const hello = (e) => {
 */}
 export default function App() {
 
-const [count, setcount] = useState(0) //variable
+const [num1, setnum1] = useState(5)
+const [count, setcount] = useState(0) //state variable
+
+function handleClick () {
+  setnum1(3)
+}
 
 const decrementFunction = () => {
   setcount(count-1)
@@ -21,13 +26,26 @@ const incrementFunction = () => {
   setcount(count+1)
 }
 
+let mynumber =10;
+
+useEffect(()=>{
+  //console.log('hi')
+  if(mynumber == 5) {
+    alert("Equal to 5")
+  }else{
+    alert("Not equal to 5")
+  }
+},[])
+
   return (
     <div id='wrapper'>
       <HeaderContent/>
       
       <BodyContent>
       <h3>Home Page</h3>
-      <p>Counter</p>
+      <button onClick={handleClick}> Click </button>
+      <num1></num1>
+      <p> {num1}</p>
       <button onClick={decrementFunction}> - </button>
       <button onClick={incrementFunction}> + </button>
       <p>{count}</p>
